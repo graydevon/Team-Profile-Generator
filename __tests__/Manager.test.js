4fcc84423534f9f3944af0334531b82287243008
Devon Gray <devongray@Devons-MacBook-Pro.local>
2022-09-20 17:13:12 -0500

    Manager test fix

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,8 +4,8 @@ const manager = new Manager("Dev","123","devondgray28@gmail.com", "233-2323");
 test('if constructor parameters are being retrieved', () => {
   expect(manager.name).toBe("Dev");
   expect(manager.id).toBe("123");
-  expect(manager.email).toBe("devondgray28@gmail.com@gmail.com");
-  expect(manager.officeNumber).toBe("867-5309");
+  expect(manager.email).toBe("devondgray28@gmail.com");
+  expect(manager.officeNumber).toBe("233-2323");
 });
 
 test('if getOfficeNumber() retrieves manager office number', () => {
