a11782fd6b6f68f71d2faad2388f5e0fc4047984
Devon Gray <devongray@Devons-MacBook-Pro.local>
2022-09-20 17:12:08 -0500

    File fix

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,17 @@
+const Manager = require('../lib/Manager');
+const manager = new Manager("Dev","123","devondgray28@gmail.com", "233-2323");
+
+test('if constructor parameters are being retrieved', () => {
+  expect(manager.name).toBe("Dev");
+  expect(manager.id).toBe("123");
+  expect(manager.email).toBe("devondgray28@gmail.com@gmail.com");
+  expect(manager.officeNumber).toBe("867-5309");
+});
+
+test('if getOfficeNumber() retrieves manager office number', () => {
+  expect(manager.getOfficeNumber()).toBe("233-2323");
+});
+
+test('if getRole() retrieves manager role', () => {
+  expect(manager.getRole()).toBe("Manager");
+});
